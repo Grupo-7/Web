cb1d7dfaecbfb66375c5fbeebc28cfa3359a009a
Carlos Rodriguez <carlosrod.furlan@gmail.com>
2015-09-12 02:43:11 +0000

    Nuevos requerimientos agregados por el product owner.

--- routes/android.js
+++ routes/android.js
@@ -29,14 +29,24 @@ router.get('/', function(req, res, next) {
     
     switch(query['op']){
         case 'asistir':
+            
             console.log('asistir');
-            var cadena = 'INSERT INTO ASISTENCIA VALUES('+query['evento']+',\''+query['carnet']+'\');';
-            connection.query(cadena, function(err,result){
-                if(err){
-                    res.render('android', { response: JSON.stringify({resultado: 'Error'}) });
+            var cadena1 = 'SELECT COUNT(carnet) AS \'count\' FROM ALUMNO WHERE carnet = \''+query['carnet']+'\';';
+            
+            connection.query(cadena1, function(err,result){
+                if(result['count'] == 0){
+                    var cadena2 = 'INSERT INTO ASISTENCIA VALUES('+query['evento']+',\''+query['carnet']+'\');';
+                    connection.query(cadena2, function(err,result){
+                        if(err){
+                            res.render('android', { response: JSON.stringify({response: 'Su carnet ya fue registrado'}) });
+                        }else{
+                            res.render('android', { response: JSON.stringify({response: 'Registro exitoso'}) });
+                        }
+                    });
                 }else{
-                    res.render('android', { response: JSON.stringify({resultado: 'Exito'}) });
+                    res.render('android', { response: JSON.stringify({response: 'Carnet no encontrado'}) });
                 }
+                
             });
             break;
             
@@ -45,9 +55,9 @@ router.get('/', function(req, res, next) {
             var cadena = 'SELECT E.id_evento, E.nombre, E.inicio, E.final, E.estado FROM EVENTO E WHERE E.estado = 1;';
             connection.query(cadena, function(err,result){
                 if(err){
-                    res.render('android', { response: JSON.stringify({resultado: 'Error'}) });
+                    res.render('android', { response: JSON.stringify({response: 'Error'}) });
                 }else{
-                    res.render('android', { response: JSON.stringify({resultado: result}) });
+                    res.render('android', { response: JSON.stringify({response: result}) });
                 }
             });
             break;    
